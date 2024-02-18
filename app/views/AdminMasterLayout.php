5b2b8ca130d5f22e4d1d0e23d581844eee370bf8
lnthai <148455849+lnthai@users.noreply.github.com>
2024-02-18 18:41:47 +0700

    Delete app/views directory

--- app/views/AdminMasterLayout.php
+++
@@ -1,4 +0,0 @@
-<?php $this->view('inc/Admin/header', $data); ?>
-<?php $this->view('inc/Admin/sidebar'); ?>
-<?php require_once "./app/Views/pages/Admin/" . $data['pages'] . ".php"; ?>
-<?php $this->view('inc/Admin/footer', $data); ?>
--- app/views/ClientMasterLayout.php
+++
@@ -1,3 +0,0 @@
-<?php $this->view('inc/Client/header', $data); ?>
-<?php require_once "./app/Views/pages/Client/" . $data['pages'] . ".php"; ?>
-<?php $this->view('inc/Client/footer', $data); ?>
